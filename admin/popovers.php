71b7588874e6a7fffa7fe40876fb30c75ddf085e
administrator@Waltahr <jpuckett@walton.uark.edu>
2016-03-09 13:26:21 -0600

    add popovers

---
+++ admin/popovers.php
@@ -0,0 +1,30 @@
+<?php
+$page_title = "Popovers";
+require_once("_resources/header.inc.php");
+?>
+
+<?php echo "<h1>$page_title</h1>"; ?>
+
+<div class='well'>
+  <a
+    title=""
+    href="javascript://gsb-dev2.uark.edu"
+    data-toggle="popover"
+    data-trigger="focus"
+    data-placement="right"
+    data-content="
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. 
+      Sed nisi. Nulla quis sem at nibh elementum imperdiet. 
+      Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. 
+      Mauris massa. Vestibulum lacinia arcu eget nulla. 
+    " 
+    data-original-title="Popover Header Text"
+  >
+    Click Here for Popover Text Box
+  </a>
+</div><!-- /.well -->
+
+<?php require_once('_resources/footer.inc.php');?>
+
+<script>$('[data-toggle="popover"]').popover()</script>
