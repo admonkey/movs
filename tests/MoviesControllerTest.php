ecd3dff34bb95545925b0324487b4541f98fca45
Jeff Puckett <jeff@jeffpuckett.com>
2016-06-01 17:05:35 -0500

    create test to scan source

--- tests/MoviesControllerTest.php
+++ tests/MoviesControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 class MoviesControllerTest extends PHPUnit_Framework_TestCase
 {
+
     public function testSourceCreated()
     {
         // Arrange
@@ -31,4 +32,44 @@ class MoviesControllerTest extends PHPUnit_Framework_TestCase
         // Assert
         $this->assertEquals($sourceData, $sourceArray);
     }
+
+    public function testSourceScanned()
+    {
+        // Arrange
+        // elevate permissions
+        $_SESSION["ADMIN"] = true;
+
+        // create object
+        $theatre = new MoviesController();
+
+        // create new source
+        $randomString = bin2hex(openssl_random_pseudo_bytes(10));
+        $sourceName = "testSource-$randomString";
+        $realSourcePath = __DIR__."/testSource";
+        $webSourcePath = "/testWebSourcePath/$randomString/testSource";
+        $sourceData = array(
+          "sourcename" => "$sourceName",
+          "realsourcepath" => "$realSourcePath",
+          "websourcepath" => "$webSourcePath"
+        );
+        $sourceID = $theatre->createSource($sourceData);
+
+
+        // Act
+        $sourceMovies = $theatre->scanSource($sourceID);
+
+
+        // Assert
+        // expect data
+        $expectedMovies = array(
+          0 => "film.avi",
+          1 => "flick.mov",
+          2 => "movie.mkv",
+          3 => "nestedFolder/deepNestedFolder/deep.avi",
+          4 => "nestedFolder/nested.mkv",
+          5 => "show.mp4"
+        );
+        $this->assertEquals($expectedMovies, $sourceMovies);
+    }
+
 }
