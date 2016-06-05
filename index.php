4378b21f7b5cedb685efba50bb3eda93d272733d
Jeff Puckett <jeff@jeffpuckett.com>
2016-06-05 15:04:02 -0500

    clear junk from index page

--- index.php
+++ index.php
@@ -10,23 +10,6 @@ echo "
   <h1>Welcome to $site_title</h1>
 ";
 
-$_SESSION["ADMIN"] = true;
-
-$theatre = new MoviesController();
-
-$sourceData = array(
-  "sourcename" => "tmp",
-  "realsourcepath" => "/tmp",
-  "websourcepath" => "/tmpweb"
-);
-
-//var_dump($theatre->createSource($sourceData));
-var_dump($theatre->getSource(131));
-
-// var_dump(bin2hex(openssl_random_pseudo_bytes(10)));
-
-
-
 require_once('_resources/footer.inc.php');
 
 ?>
