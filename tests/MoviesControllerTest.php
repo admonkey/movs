0f366d7d4cd58fef179d9580f76a8840cd6f2d48
Jeff Puckett <jeff@jeffpuckett.com>
2016-06-05 12:17:13 -0500

    create test dependencies, separate source retrieval, and make model static

--- tests/MoviesControllerTest.php
+++ tests/MoviesControllerTest.php
@@ -1,41 +1,92 @@
 <?php
+define('TEST_SOURCE_DIR',__DIR__.'/testSource');
 class MoviesControllerTest extends PHPUnit_Framework_TestCase
 {
+
+    private static $sourceID = 1;
+    private static $sourceData = array(
+      "sourcename" => "testSource",
+      "realsourcepath" => TEST_SOURCE_DIR,
+      "websourcepath" => "/testWebSourcePath"
+    );
+    private static $expectedMovies = array(
+      0=>
+      array(
+        "fpath"=>"film.avi",
+        "fname"=>"film.avi"
+      ),
+      1=>
+      array(
+        "fpath"=>"flick.mov",
+        "fname"=>"flick.mov"
+      ),
+      2=>
+      array(
+        "fpath"=>"movie.mkv",
+        "fname"=>"movie.mkv"
+      ),
+      3=>
+      array(
+        "fpath"=>"nestedFolder/deepNestedFolder/deep.avi",
+        "fname"=>"deep.avi"
+      ),
+      4=>
+      array(
+        "fpath"=>"nestedFolder/nested.mkv",
+        "fname"=>"nested.mkv"
+      ),
+      5=>
+      array(
+        "fpath"=>"show.mp4",
+        "fname"=>"show.mp4"
+      )
+    );
 
     public function testSourceCreated()
     {
         // Arrange
         // elevate permissions
-        echo PHP_EOL."Setting ADMIN = TRUE.".PHP_EOL;
         $_SESSION["ADMIN"] = true;
 
         // create object
         $theatre = new MoviesController();
 
-        // package new source data
-        $randomString = bin2hex(openssl_random_pseudo_bytes(10));
-        $sourceName = "testSource-$randomString";
-        $realSourcePath = __DIR__;
-        $webSourcePath = "/testWebSourcePath/$randomString";
-        $sourceData = array(
-          "sourcename" => "$sourceName",
-          "realsourcepath" => "$realSourcePath",
-          "websourcepath" => "$webSourcePath"
-        );
-
         // Act
         // create source
         echo PHP_EOL."Creating source.".PHP_EOL;
-        $sourceID = $theatre->createSource($sourceData);
+        $sourceID = $theatre->createSource(self::$sourceData);
+        var_dump($sourceID);
 
-        // get source
-        echo PHP_EOL."Getting source $sourceID.".PHP_EOL;
-        $sourceArray = $theatre->getSource($sourceID);
 
         // Assert
-        $this->assertEquals($sourceData, $sourceArray);
+        $this->assertEquals($sourceID, self::$sourceID);
     }
 
+    /**
+     * @depends testSourceCreated
+     */
+    public function testSourceRetrieved()
+    {
+        // Arrange
+        // elevate permissions
+        $_SESSION["ADMIN"] = true;
+
+        // create object
+        $theatre = new MoviesController();
+
+        // Act
+        // get source
+        echo PHP_EOL."Getting source.".PHP_EOL;
+        $sourceArray = $theatre->getSource(self::$sourceID);
+        var_dump($sourceArray);
+
+        // Assert
+        $this->assertEquals(self::$sourceData, $sourceArray);
+    }
+
+    /**
+     * @depends testSourceRetrieved
+     */
     public function testSourceScanned()
     {
         // Arrange
@@ -45,58 +96,14 @@ class MoviesControllerTest extends PHPUnit_Framework_TestCase
         // create object
         $theatre = new MoviesController();
 
-        // create new source
-        $randomString = bin2hex(openssl_random_pseudo_bytes(10));
-        $sourceName = "testSource-$randomString";
-        $realSourcePath = __DIR__."/testSource";
-        $webSourcePath = "/testWebSourcePath/$randomString/testSource";
-        $sourceData = array(
-          "sourcename" => "$sourceName",
-          "realsourcepath" => "$realSourcePath",
-          "websourcepath" => "$webSourcePath"
-        );
-        $sourceID = $theatre->createSource($sourceData);
-
-
         // Act
-        $sourceMovies = $theatre->scanSource($sourceID);
+        echo PHP_EOL."Scanning source.".PHP_EOL;
+        $sourceMovies = $theatre->scanSource(self::$sourceID);
+        var_dump($sourceMovies);
 
 
         // Assert
-        // expect data
-        $expectedMovies = array(
-          0=>
-          array(
-            "fpath"=>"film.avi",
-            "fname"=>"film.avi"
-          ),
-          1=>
-          array(
-            "fpath"=>"flick.mov",
-            "fname"=>"flick.mov"
-          ),
-          2=>
-          array(
-            "fpath"=>"movie.mkv",
-            "fname"=>"movie.mkv"
-          ),
-          3=>
-          array(
-            "fpath"=>"nestedFolder/deepNestedFolder/deep.avi",
-            "fname"=>"deep.avi"
-          ),
-          4=>
-          array(
-            "fpath"=>"nestedFolder/nested.mkv",
-            "fname"=>"nested.mkv"
-          ),
-          5=>
-          array(
-            "fpath"=>"show.mp4",
-            "fname"=>"show.mp4"
-          )
-        );
-        $this->assertEquals($expectedMovies, $sourceMovies);
+        $this->assertEquals(self::$expectedMovies, $sourceMovies);
     }
 
 }
